c681e59f4e1a0c8365bd16f959493759ef63821c
mike@stein <mike@stein>
2017-01-18 18:04:13 +0000

    yup update

--- manifest/kubmjsmas.pp
+++ manifest/kubmjsmas.pp
@@ -2,6 +2,7 @@ class { 'docker':
     package_name => 'docker-1.10.3-59.el7.centos.x86_64', 
     extra_parameters => "--insecure-registry 192.168.50.4:5000",
 }
+contain 'docker'
 docker::image { 'registry:2':
 }
 docker::run { 'registry':
@@ -11,6 +12,11 @@ docker::run { 'registry':
 }
 docker::image {'192.168.50.4:5000/ruby_app:2':
   docker_dir => '/vagrant/plain_kuber/images/ruby_web/',
+}~>
+exec{'docker push 192.168.50.4:5000/ruby_app:2':
+  refreshonly => true,
+  path        => ['/bin'],
+  require     =>  Docker::Run['registry'],
 }
     class {'kubernetes::master':
       master_name                      => '192.168.50.4', #can be hostname if dns setup
@@ -20,4 +26,23 @@ docker::image {'192.168.50.4:5000/ruby_app:2':
       manage_docker                    => false,
       alternate_flannel_interface_bind => 'eth1'
     }
+    contain 'kubernetes::master'
 
+    exec{'kubectl create -f /vagrant/plain_kuber/pods/dns/skydnssvc.yaml':
+      path  => ['/bin'],
+      require =>  [Class['kubernetes::master'],Exec['docker push 192.168.50.4:5000/ruby_app:2']]
+    }
+    exec{'kubectl create -f /vagrant/plain_kuber/pods/dns/skydnsrc.yaml':
+      path  => ['/bin'],
+      require =>  [Class['kubernetes::master'],Exec['docker push 192.168.50.4:5000/ruby_app:2']]
+    }
+
+    exec{'kubectl create -f /vagrant/plain_kuber/pods/ruby_web/ruby_web.yaml':
+      path    => ['/bin'],
+      require =>  [Class['kubernetes::master'],Exec['docker push 192.168.50.4:5000/ruby_app:2']]
+    }
+
+    exec{'kubectl create -f /vagrant/plain_kuber/pods/ruby_web/ruby_web_service.yaml':
+      path  => ['/bin'],
+      require =>  [Class['kubernetes::master'],Exec['docker push 192.168.50.4:5000/ruby_app:2']]
+    }
--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 setenforce 0
 service firewalld stop
 rpm -Uvh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
-yum install puppetserver -y 
+yum install puppetserver -y
+
